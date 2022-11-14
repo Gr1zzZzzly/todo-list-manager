b513abbeeb9f512722961019ba1d81bf25cf77ed
igor kosianenko <funkcionalkos@gmail.com>
2022-11-14 13:08:28 +0200

    Solution)

--- todo/models.py
+++ todo/models.py
@@ -7,12 +7,16 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+
 class Task(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    deadline = models.DateField(blank=True)
+    deadline = models.DateField(default=None)
     done = models.BooleanField(default=False)
     tags = models.ManyToManyField(to=Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["done", "created_at"]
 
     def __str__(self):
         return f"{self.content} {self.done}"
